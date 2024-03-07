58a4d9b120386af74477566e701ed17c33c39d5f
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-03-07 19:45:20 +0200

    public

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', [PublicController::class, 'index'])->name('landipage');
+Route::get('shows-and-events', [PublicController::class, 'showEvents'])->name('showevent');
+
 
 Route::middleware([
     'auth:sanctum',
