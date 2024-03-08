eac112cf444aee75b5bd4e7c01c9bd9ca8eb7a3d
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-03-08 22:45:42 +0200

    i am done

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,12 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Event extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('topic')
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(50);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
 }
--- database/migrations/2024_03_08_195218_create_registrations_table.php
+++ database/migrations/2024_03_08_195218_create_registrations_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('registrations', function (Blueprint $table) {
             $table->id();
+            $table->integer('event_id');
+            $table->string('name')->nullable();
+            $table->longText('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('company')->nullable();
+            $table->date('position')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+
+
             $table->timestamps();
         });
     }
