5d47a6a5369024808582def354e11fa22815ade2
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-03-21 07:36:27 +0200

    matweeb and pdf

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -203,7 +203,8 @@
                                                             @if ($registration->count() > 0)
                                                                 <h4>Total : {{ $registration->count() }}</h4>
                                                                 <br>
-                                                                <a href="{{ route('events.regtable', $event) }}" class="btn btn-primary btn-sm">See full
+                                                                <a href="{{ route('events.regtable', $event) }}"
+                                                                    class="btn btn-primary btn-sm">See full
                                                                     details</a>
                                                                 <br>
                                                                 <br>
